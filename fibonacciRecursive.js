5941f44463eab45cb5cd1aa6ef992723d232f4ae
Nahidjc <nahidjc80@gmail.com>
2021-06-22 22:55:49 +0600

    fibonacciRecursive

---
+++ fibonacciRecursive.js
@@ -0,0 +1,11 @@
+function fibonacciRecursive(n) {
+    if (n == 0) {
+        return 0
+    } if (n == 1) {
+        return 1
+    } else {
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+}
+const fibo = fibonacciRecursive(15)
+console.log(fibo);
