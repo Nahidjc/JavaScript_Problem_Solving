1a572c7c6c21207123f8b716eb74992fb7059850
Nahidjc <nahidjc80@gmail.com>
2021-06-23 22:41:46 +0600

    reverse

---
+++ reverse.js
@@ -0,0 +1,8 @@
+var sentence = "This is Nahid Hasan.";
+var reverse = []
+for (let i = 0; i < sentence.length; i++) {
+    element = sentence[i];
+    reverse = element + reverse;
+
+}
+console.log(reverse);
